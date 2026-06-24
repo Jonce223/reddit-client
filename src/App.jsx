564a09f365a3270fc10e71e643fa0d4eff28178c
Jonas <joncevas@Jonass-MacBook-Air.local>
2026-06-24 19:47:09 +0200

    Converted hardcoded posts into a dynamic map array using mock data

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,35 @@ import './App.css'
 function App() {
   const dispatch = useDispatch(); //Allows to send messages to the user
 
+  const posts = [
+    {
+      id: '1',
+      title: 'Look At this amazing set up I build for myself',
+      username: 'john_doe',
+      upvotes: '1.2k',
+      commentsCount: 42,
+      hasImage: true
+    },
+    {
+      id: '2',
+      title: 'Had the best Pizza in town!',
+      username: 'javascript is fun',
+      upvotes: '500',
+      commentsCount: 14,
+      hasImage: false,
+      body: 'I found this amazing pizza place downtown. The crust was perfect and the topics amazing!'
+    },
+    {
+      id: '3',
+      title: 'React Hooks vs Redux Toolkit - what are your thoughts?',
+      username: 'redux_master',
+      upvotes: '300',
+      commentsCount: 8,
+      hasImage: false,
+      body: 'I a building a Reddit clients and love how Redux Toolkit is hard'
+    }
+  ];
+
   //Reading from redux and the window must be open
   const isModalOpen= useSelector((state) => state.ui.isModalOpen);
 
@@ -39,49 +68,41 @@ function App() {
       {/* Posts Container */}
       <main className= "posts-container">
 
-            {/* Post 1 */}
-          <div className="post-card">
-            {/*1. Voting place*/}
+        {posts.map((post) => (
+          <div className='post-card' key={post.id}>
+
+            {/** 1. Voting part */}
             <div className='post-votes'>
               <button className='vote-btn'>▲</button>
-              <span className='vote-count'>1.2k</span>
+              <span className='vote-count'>{post.upvotes}</span>
               <button className='vote-btn'>▼</button>
             </div>
-          
 
-            {/* Post content (text and place for image) */}
-            <div className="post-content">
-              <p className='post-meta'>Posted by u/john_doe</p>
-              <h2 className='post-title'>Look At this amazing set up I build for my self</h2>
-              <div className='post-media-placeholder'>
-                <span> [ Image / Media Placeholder ] </span>
-              </div>
+            {/** 2. Content Part */}
+            <div className='post-content'>
+              <p className='post-meta'>Posted by /u{post.username}</p>
+              <h2 className='post-title'>{post.title}</h2>
+
+              {/** If the text has image, we display it */}
+              {post.hasImage && (
+                <div className='post-media-placeholder'>
+                  <span> [ Image / Media Placejolder </span>
+                </div>
+              )}
+
+              {/** if Post has text we display it */}
+              {post.body && <p className='post-text-body'>{post.body}</p>}
             </div>
 
-            {/*Place with comments */}
-            <div className="post-footer">
-              <button className='comment-btn' onClick={() => dispatch(openModal())}>💬 42 Comments</button>
+            <div className='poster-footer'>
+              <button className='comment-btn' onClick={() => dispatch(openModal())}>
+                💬 {post.commentsCount} Comments
+              </button>
             </div>
-          </div>
 
-        {/*Post 2 (Simpler just with text) */}
-        <div className="post-card">
-          <div className='post-votes'>
-            <button className='vote-btn'>▲</button>
-            <span className='vote-count'>500</span>
-            <button className='vote-btn'>▼</button>
           </div>
+        ))}
 
-          <div className="post-content">
-            <p className='post-meta'>Posted by u/javascript_fan</p>
-            <h2 className='post-title'>Just had the best pizza in town!</h2>
-            <p className='post-text-body'>I found this amazing pizza place downtown. The crust was perfect and the toppings were fresh. Highly recommend it to all pizza lovers!</p>
-          </div>
-
-          <div className="post-footer">
-            <button className='comment-btn' onClick={() => dispatch(openModal())}>💬 15 Comments</button>
-          </div>
-        </div>
       </main>
 
       {/*Sidebar */}
